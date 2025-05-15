d977a72b7730a421c406c675e97bc095a0e83107
Nrv <rushansaiyed23@gmail.com>
2025-05-15 17:05:33 +0530

    commit

---
+++ twodimensionalarraylist.java
@@ -0,0 +1,31 @@
+    import java.util.*;
+public class twodimensionalarraylist {
+
+
+
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+
+        // Add 3 rows
+        for (int i = 0; i < 3; i++) {
+            list.add(new ArrayList<>());
+        }
+
+        // Fill data
+        int num = 1;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                list.get(i).add(num++);
+            }
+        }
+
+        // Print the 2D ArrayList
+        for (ArrayList<Integer> row : list) {
+            for (int val : row) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
--- twodimensionmax.java
+++ twodimensionmax.java
@@ -17,8 +17,9 @@ public class twodimensionmax {
             for (int j = 0; j < 3; j++) {
                 System.err.print(" "+arr[i][j]);
             }
+            System.err.println();
     }
-
+//max min
     System.err.println("");
      for (int z = 0; z < 3; z++) {
             for (int j = 0; j < 3; j++) {
@@ -33,5 +34,51 @@ public class twodimensionmax {
         
         System.err.println("The maximum element in the 2D array is: " + max);
         sc.close();
+        System.err.println();
+//transpose matrix
+        int m=arr.length;
+        int n=arr[0].length;
+          for (int j = 0; j < n; j++) {
+            for (int i = 0; i < m; i++) {
+                System.err.print(" "+arr[i][j]);
+            }
+            System.err.println();
+
 }
+System.err.println();
+//90 degree rotate
+for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n/2; j++) {
+                int temp = arr[i][j];
+                arr[i][j] = arr[n - j - 1][i];
+                arr[n - j - 1][i] = temp;
+            }
+            }
+             for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.err.print(" "+arr[i][j]);
+            }
+            System.err.println();
+             
+
     }
+    // wave zig zag
+System.err.println();
+int z = arr.length;        // number of rows
+int y = arr[0].length;     // number of columns
+
+for (int i = 0; i < z; i++) {
+    if (i % 2 == 0) {
+        // even row: left to right
+        for (int j = 0; j < y; j++) {
+            System.out.print(arr[i][j] + " ");
+        }
+    } else {
+        // odd row: right to left
+        for (int j = y - 1; j >= 0; j--) {
+            System.out.print(arr[i][j] + " ");
+        }
+    }
+}
+}
+}
